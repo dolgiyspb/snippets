4cb2e399ad71f37e94e9a4a5066ca8f69319a827
Alexander.Dolgopolov <Alexander.Dolgopolov@acronis.com>
2016-12-29 19:16:38 +0300

    Rename

---
+++ snippets.sh
@@ -0,0 +1,1 @@
+grep -e "Collected statistics from cluster" celery_periodic.log | cut -d' ' -f 2 | sed -e 's/,[0-9]*:$//' | python3 -c "import sys,datetime; a=[datetime.datetime.strptime(l.strip(),'%H:%M:%S') for l in sys.stdin.readlines()];print(list(map(lambda n: (n[1] - n[0]).total_seconds(), filter(lambda tp: len(tp) == 2, map(lambda si: a[si:si + 2], range(0, len(a), 2))))));"
